f03e76eebf0506316dfd9e90ba116bf44730e823
Hürrem ÖMÜR <144717555+hurremomur@users.noreply.github.com>
2025-08-22 20:38:27 +0300

    UserRoleRepository dosyası eklendi.

---
+++ src/main/java/com/floproje/StokYonetim/repository/UserRoleRepository.java
@@ -0,0 +1,11 @@
+package com.floproje.StokYonetim.repository;
+
+import com.floproje.StokYonetim.entity.UserRole;
+import com.floproje.StokYonetim.enums.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+//authservice için
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    Optional<UserRole> findByRole(Role role);
+}
